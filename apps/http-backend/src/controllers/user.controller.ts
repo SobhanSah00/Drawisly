0831eb126a181d5a215699b9ca727d493fb7f393
sobhan9012 <sobhansahoo2000@gmail.com>
2025-07-20 17:05:09 +0530

    feat : whole setup is done

---
+++ apps/http-backend/src/controllers/user.controller.ts
@@ -0,0 +1,3 @@
+import {UserSignUpSchema} from "@repo/common/type"
+import {prisma} from "@repo/db/client"
+
---
+++ packages/common/types.ts
@@ -0,0 +1,35 @@
+import { z } from "zod"
+
+export const UserSignUpSchema = z.object({
+    username : z.string().min(4).max(8),
+    email : z.email(),
+    password : z.string().min(6).max(10),
+})
+
+export const UserSignInSchema = z.object({
+    username : z.string(),
+    password : z.string().min(6)
+})
+
+export const CreateRoomSchema = z.object({
+    title : z.string()
+})
+
+export const JoinRoomSchema = z.object({
+    joinCode : z.string().length(8)
+})
+
+export const WebSocketMessageSchema = z.object({
+    type : z.enum([
+        "connect_room",
+        "disconnect_room",
+        "chat_message",
+        "draw",
+        "error_message"
+    ]),
+    roomId : z.string(),
+    userId : z.string(),
+    content : z.string().optional()
+})
+
+export type WebSocketMessage = z.infer<typeof WebSocketMessageSchema>
