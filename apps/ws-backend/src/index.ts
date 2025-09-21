561b85fc04b631f2466755d7205a621b73cf6cdd
sobhan9012 <sobhansahoo2000@gmail.com>
2025-09-21 22:42:10 +0530

    ..

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,6 +1,6 @@
 import { WebSocket, WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({port : 8090})
+const wss = new WebSocketServer({port : 8080})
 
 wss.on('connection', function connection(socket) {
     socket.on('error', console.error)
